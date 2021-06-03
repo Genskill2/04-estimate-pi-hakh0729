8f5d53ec21d4eb3ec045d3b125cfe048a6638d69
hakh0729 <84660713+hakh0729@users.noreply.github.com>
2021-06-03 14:00:48 +0530

    upt wallis.c

--- wallis.c
+++ wallis.c
@@ -23,12 +23,13 @@ int main(void) {
     }
   }
 }
+
 float wallis_pi(int n)
-{
-  double pc=1.0;
-  for(int i=1;i<= n;i++)
-  {
-    pc=pc*((2*i/(2*i-1.0))*(2*i/(2*i+1.0)));
-  }
-  return pc;
-}
+ {
+    float s=1.0;
+    for (int i=1; i<=n; i++)
+    {
+        s=s*(4*i*i)/((4*i*i)-1);
+    }
+   return (2*s); 
+ }
